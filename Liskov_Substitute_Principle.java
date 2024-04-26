ac0f8c0889dc86d89fe72214ecbb5b21fd38a182
mohaiminulshaad <158316586+mohaiminulshaad@users.noreply.github.com>
2024-04-26 20:57:23 +0600

    Liskov_Substitute_Principle.java

--- Liskov_Substitute_Principle.java
+++ Liskov_Substitute_Principle.java
@@ -1,21 +1,35 @@
 /**
  * @author Mohaiminul Islam Shaad; Student ID : 220201
- * @version 1.0
+ * @version 2.0
  * @since 2024-04-22
  */
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Interface representing a football entity.
+ */
+interface FootballEntity {
+    /**
+     * Gets the name of the football entity.
+     *
+     * @return The name of the football entity.
+     */
+    String getName();
+}
+
+/**
  * Player class representing a football player.
  */
-class Player {
+class Player implements FootballEntity {
     private String name;
     private int jerseyNumber;
 
     /**
-     * Constructor to create a new Player object.
-     * @param name The name of the player.
+     * Creates a new Player instance with the given name and jersey number.
+     *
+     * @param name         The name of the player.
      * @param jerseyNumber The jersey number of the player.
      */
     public Player(String name, int jerseyNumber) {
@@ -23,10 +37,16 @@ class Player {
         this.jerseyNumber = jerseyNumber;
     }
 
+    @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the jersey number of the player.
+     *
+     * @return The jersey number of the player.
+     */
     public int getJerseyNumber() {
         return jerseyNumber;
     }
@@ -40,12 +60,13 @@ class Player {
 /**
  * Team class representing a football team.
  */
-class Team {
+class Team implements FootballEntity {
     private String name;
     private List<Player> players;
 
     /**
-     * Constructor to create a new Team object.
+     * Creates a new Team instance with the given name.
+     *
      * @param name The name of the team.
      */
     public Team(String name) {
@@ -54,17 +75,24 @@ class Team {
     }
 
     /**
-     * Add a player to the team.
-     * @param player The player to be added.
+     * Adds a player to the team.
+     *
+     * @param player The player to add to the team.
      */
     public void addPlayer(Player player) {
         players.add(player);
     }
 
+    /**
+     * Gets the list of players in the team.
+     *
+     * @return The list of players in the team.
+     */
     public List<Player> getPlayers() {
         return players;
     }
 
+    @Override
     public String getName() {
         return name;
     }
@@ -84,22 +112,24 @@ class Team {
  * Match class representing a football match between two teams.
  */
 class Match {
-    private Team homeTeam;
-    private Team awayTeam;
+    private FootballEntity homeTeam;
+    private FootballEntity awayTeam;
     private String result;
 
     /**
-     * Constructor to create a new Match object.
-     * @param homeTeam The home team.
-     * @param awayTeam The away team.
+     * Creates a new Match instance with the given home and away teams.
+     *
+     * @param homeTeam The home team for the match.
+     * @param awayTeam The away team for the match.
      */
-    public Match(Team homeTeam, Team awayTeam) {
+    public Match(FootballEntity homeTeam, FootballEntity awayTeam) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
     }
 
     /**
-     * Set the result of the match.
+     * Sets the result of the match.
+     *
      * @param result The result of the match.
      */
     public void setResult(String result) {
@@ -113,29 +143,28 @@ class Match {
 }
 
 /**
- * Main class to test the football club system.
+ * Main class to demonstrate the football entities and a match.
  */
 public class Liskov_Substitute_Principle {
+    /**
+     * Main method to create football entities, a match, and display the results.
+     *
+     * @param args The command-line arguments (not used in this program).
+     */
     public static void main(String[] args) {
-        // Create players
-        Player player1 = new Player("Lionel Messi", 10);
-        Player player2 = new Player("Cristiano Ronaldo", 7);
+        // Creating a Player object
+        FootballEntity player = new Player("Lionel Messi", 10);
 
-        // Create teams and add players
         Team barcelona = new Team("Barcelona");
-        barcelona.addPlayer(player1);
+        barcelona.addPlayer((Player) player); // Casting to Player type
 
         Team realMadrid = new Team("Real Madrid");
-        realMadrid.addPlayer(player2);
+        realMadrid.addPlayer(new Player("Cristiano Ronaldo", 7)); // Directly adding a Player
 
-        // Create a match between two teams
         Match match = new Match(barcelona, realMadrid);
-        match.setResult("2-3"); // Example result
+        match.setResult("2-3");
 
-        // Print match result
         System.out.println(match);
-
-        // Print team information
         System.out.println(barcelona);
         System.out.println(realMadrid);
     }
